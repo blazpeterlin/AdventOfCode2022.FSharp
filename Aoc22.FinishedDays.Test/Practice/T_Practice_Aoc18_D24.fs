76494d63f6bd15d63148340101dde51fbc13c2ea
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-12-01 06:09:36 +0100

    Day 1

--- Aoc22.FinishedDays.Test/Practice/T_Practice_Aoc18_D24.fs
+++ Aoc22.FinishedDays.Test/Practice/T_Practice_Aoc18_D24.fs
@@ -1,25 +1,25 @@
-﻿module Aoc22.FinishedDays.Test.T_Aoc18_D24
-open Aoc22.Practice_Aoc18_D24
+﻿//module Aoc22.FinishedDays.Test.T_Aoc18_D24
+//open Aoc22.Practice_Aoc18_D24
 
-open Aoc22.Input
-open NUnit.Framework
-open System
+//open Aoc22.Input
+//open NUnit.Framework
+//open System
 
-[<SetUp>]
-let Setup () =
-    ()
+//[<SetUp>]
+//let Setup () =
+//    ()
 
-let basePath = "Practice"
-let prependFolder fname = basePath + "\\" + fname
+//let basePath = "Practice"
+//let prependFolder fname = basePath + "\\" + fname
     
-[<TestCase("input-TEST.txt", 28976)>] 
-let Test1 (fn : string, res: int) = 
-    let input = fn |> prependFolder |> f2text
-    let sln1 = solve1 input
-    Assert.That(sln1, Is.EqualTo res)
+//[<TestCase("input-TEST.txt", 28976)>] 
+//let Test1 (fn : string, res: int) = 
+//    let input = fn |> prependFolder |> f2text
+//    let sln1 = solve1 input
+//    Assert.That(sln1, Is.EqualTo res)
     
-[<TestCase("input-TEST.txt", 3534)>] 
-let Test2 (fn : string, res: int) = 
-    let input = fn |> prependFolder |> f2text
-    let sln2 = solve2 input
-    Assert.That(sln2, Is.EqualTo res)
+//[<TestCase("input-TEST.txt", 3534)>] 
+//let Test2 (fn : string, res: int) = 
+//    let input = fn |> prependFolder |> f2text
+//    let sln2 = solve2 input
+//    Assert.That(sln2, Is.EqualTo res)
---
+++ Aoc22.FinishedDays/D01.fs
@@ -0,0 +1,52 @@
+﻿module Aoc22.D01
+
+open Aoc22
+open Input
+open TextCopy
+open System.Collections.Generic
+open Aoc22.Operators
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Text.RegularExpressions
+open System
+open FSharp.Text.RegexProvider
+open FSharp.Text.RegexExtensions
+
+let input =  "input.txt" |> f2text
+
+let parseLine (ln:string) =
+    if ln = "" then None
+    else Some (ln |> int)
+    // |> text2tokens "x"
+    // |> text2tokensStr ["abc";"def"]
+
+let parse2lines (text:string) = 
+    text
+    |> text2lines 
+    |> List.map parseLine
+
+let solve1 (text:string) = 
+    let inp = text |> parse2lines
+    let grps : int list list = 
+          inp 
+          |> List.fold (fun (x::y) item -> if item = None then []::x::y else ((item.Value)::x)::y) [[]]
+
+    let max = grps |> map (List.sum) |> List.max
+    max
+    
+let solve2 (text:string) =
+    let inp = text |> parse2lines
+    let grps : int list list = 
+          inp 
+          |> List.fold (fun (x::y) item -> if item = None then []::x::y else ((item.Value)::x)::y) [[]]
+
+    let max3 = grps |> map (List.sum) |> List.sortDescending |> List.take 3 |> List.sum
+    max3
+
+let res1 = input |> solve1
+let res2 = input |> solve2
+
+ClipboardService.SetText(res2.ToString())
+
+
+let finished = true
+()
