13557ddbcd3a2e90c0d4c1cb9d6f690577052122
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-12-08 17:32:52 +0100

    Optimized day 8

--- Aoc22.FinishedDays/D08.fs
+++ Aoc22.FinishedDays/D08.fs
@@ -100,7 +100,7 @@ let solve1 (text:string) =
     r
 
 let countNumTrees (mp: Map<(int*int),int>) (x0,y0) (ix,iy) (edgeX,edgeY) =
-    let coords : (int*int) list = 
+    let coords : (int*int) seq = 
         (x0,y0)
         |> Seq.unfold (fun ((x,y)) -> 
                 if (x,y) = (edgeX,edgeY) 
@@ -111,22 +111,22 @@ let countNumTrees (mp: Map<(int*int),int>) (x0,y0) (ix,iy) (edgeX,edgeY) =
                     let r2 = (x2,y2)
                     Some (r2,r2)
             )
-        |> List.ofSeq 
+        //|> List.ofSeq 
     let blockedValue = mp[x0,y0]
     let forLine = 
         coords
-        |> List.fold (fun (isBlocked,lst) (x,y) -> 
+        |> Seq.scan (fun (isBlocked,lst) (x,y) -> 
             let nextElt = mp[x,y]
-            let nextLst = if (not isBlocked) (*&& nextElt <= blockedValue*) then (x,y)::lst else lst
-            let nextIsBlocked = isBlocked || nextElt >= blockedValue
+            let nextLst = if (isBlocked=0) (*&& nextElt <= blockedValue*) then (x,y)::lst else lst
+            let nextIsBlocked = if isBlocked=1 then 2 elif nextElt >= blockedValue then 1 else 0
             nextIsBlocked,nextLst
-        ) (false,[])
+        ) (0,[])
+        |> Seq.takeWhile (fun (isBlocked, _) -> isBlocked<=1)
+        |> Seq.last
     let numTrees = forLine |> snd |> List.length
     numTrees
 
-let scenicScore (mp: Map<(int*int),int>) (x0,y0) =
-    let maxY = mp |> Map.keys |> List.ofSeq |> List.map snd |>List.max
-    let maxX = mp |> Map.keys |> List.ofSeq |> List.map fst |>List.max
+let scenicScore (mp: Map<(int*int),int>) maxX maxY (x0,y0) =
 
     //let test = if x0 =2 && y0 = 3 then 1 else 0
 
@@ -141,10 +141,12 @@ let solve2 (text:string) =
     let inp = text |> parse2lines
     let mp = inp |> Map.ofList
     let allCoords = mp |> Map.keys |> List.ofSeq
+    let maxX = allCoords |> List.map fst |>List.max
+    let maxY = allCoords |> List.map snd |>List.max
 
     let r =
         allCoords
-        |> map (scenicScore mp)
+        |> map (scenicScore mp maxX maxY)
         |> List.max
 
     r
