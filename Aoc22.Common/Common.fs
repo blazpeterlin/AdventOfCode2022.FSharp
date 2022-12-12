c8ced43a1dbd5754699c3472736541171b769c8f
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-12-12 22:22:32 +0100

    simple A* implemented

--- Aoc22.Common/Common.fs
+++ Aoc22.Common/Common.fs
@@ -29,7 +29,7 @@ module Common =
 
     
     let Dijkstra (pos0:'a) (getNextPos:'a->'a seq) (isFinish:'a->bool) =
-        let pq = PriorityQueue()
+        let pq = PriorityQueue() // mutable
         pq.Enqueue((pos0,[pos0]), 0)
         let visited0 = [] |> HashSet // mutable
         let res =
@@ -40,10 +40,28 @@ module Common =
                 if v.Add currPos then
                     let allNextPos = currPos |> getNextPos |> Seq.filter (v.Contains >> not)
                     pq.EnqueueRange (allNextPos |> Seq.map (fun pos -> (pos,pos::history),history.Length))
-                    Some((v, reportPos), v)
-                else
-                    Some((v, reportPos), v)
+                Some((v, reportPos), v)
             )
             |> Seq.choose snd
             |> Seq.head
         res
+    
+    let AStar (pos0:'a) (heuristic: 'a->int) (isFinish:'a->bool) (getNextPos:'a->('a*int) seq) =
+        let pq = PriorityQueue() // mutable
+        let h0 = heuristic pos0
+        pq.Enqueue((pos0,[pos0],0), 0+h0)
+        let v = [] |> HashSet // mutable
+        let resHistory,resCost =
+            seq {
+                while pq.Count>0 do
+                    let currPos,history,costSoFar = pq.Dequeue()
+                    if v.Add currPos then
+                        if isFinish currPos then yield (history,costSoFar)
+                        let allNextPos = currPos |> getNextPos// |> Seq.filter (fun (pos,_) -> v.Contains pos |> not)
+                        pq.EnqueueRange (allNextPos 
+                                            |> Seq.map (fun (pos,cost) -> 
+                                                            let h = heuristic pos
+                                                            (pos,pos::history,costSoFar+cost),costSoFar+cost+h))
+            }
+            |> Seq.head
+        resHistory,resCost
