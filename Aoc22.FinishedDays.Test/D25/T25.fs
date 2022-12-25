77998698f7c8581646b9aec2cbe00c028633e4be
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-12-25 06:53:35 +0100

    Day 25

---
+++ Aoc22.FinishedDays.Test/D25/T25.fs
@@ -0,0 +1,19 @@
+﻿module Aoc22.FinishedDays.Test.T25
+open Aoc22.D25
+
+open Aoc22.Input
+open NUnit.Framework
+open System
+
+[<SetUp>]
+let Setup () =
+    ()
+
+let basePath = System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name.Replace("T","D")
+let prependFolder fname = basePath + "\\" + fname
+    
+[<TestCase("input-TEST.txt", "2=--00--0220-0-21==1")>] 
+let Test1 (fn : string, res: string) = 
+    let input = fn |> prependFolder |> f2text
+    let sln1 = solve1 input
+    Assert.That(sln1, Is.EqualTo res)
---
+++ Aoc22.FinishedDays/D25.fs
@@ -0,0 +1,74 @@
+﻿module Aoc22.D25
+
+open Aoc22
+open Input
+open TextCopy
+open System.Collections.Generic
+open Aoc22.Operators
+open Microsoft.FSharp.Core.Operators.Checked
+open System.Text.RegularExpressions
+open System
+open FSharp.Text.RegexProvider
+open FSharp.Text.RegexExtensions
+
+//let input =  "input.txt" |> f2text
+
+let parseLine (ln:string) =
+    ln.ToCharArray() 
+    |> List.ofSeq 
+    |> List.map (function | '2' -> 2L | '1' -> 1L | '0' -> 0L | '-' -> -1L | '=' -> -2L)
+    |> List.fold (
+        fun num dgt ->
+            num*5L + dgt
+    ) 0L
+
+let backwards (num0: int64) =
+    let step (num:int64) acc =
+        let lastDigit = num%5L |> fun x -> if x > 2L then x-5L else x
+        let addedAcc = 
+            match lastDigit with
+            | 0L -> "0"
+            | 1L -> "1"
+            | 2L -> "2"
+            | -1L -> "-"
+            | -2L -> "="
+        num/5L + (addedAcc |> function | "-" -> 1L | "=" -> 1L | _ -> 0L), addedAcc::acc
+
+    let finalSt =
+        {| Num=num0; Acc=[] |}
+        |> Seq.unfold (
+            fun obj ->
+                if obj.Num=0L 
+                then None 
+                else
+                    let (nextNum,nextAcc) = step obj.Num obj.Acc
+                    let r = {|Num=nextNum;Acc=nextAcc|}
+                    Some(r,r)
+        )
+        |> Seq.last
+    let r2 = finalSt.Acc
+    let r3 = r2
+    let r4 = r3 |> List.reduce (+)
+
+    r4
+
+
+
+let parse2lines (text:string) = 
+    text
+    |> text2lines 
+    |> List.map parseLine
+
+let solve1 (text:string) = 
+    let inp = text |> parse2lines
+    let sm = inp |> List.sum
+    let snafu = backwards sm
+    snafu
+
+//let res1 = input |> solve1
+
+//ClipboardService.SetText(res1.ToString())
+
+
+let finished = true
+()
